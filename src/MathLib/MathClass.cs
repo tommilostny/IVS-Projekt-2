3e981b5b163a383b9100285cfa1fd26c48fb2297
rivolacz <m.rivola@seznam.cz>
2020-03-25 17:41:00 +0100

    Add prime number function.

--- src/MathLib/MathClass.cs
+++ src/MathLib/MathClass.cs
@@ -7,5 +7,26 @@ namespace MathLib
         public MathClass() { }
 
         //Zde doplňte vaše matematické funkce
+        //Return first prime number after given number.
+        public long FirstPrimeNumberAfterNumber(long number){
+            if(number < 1) number = 1;  //Negative numbers are not prime numbers
+            if(number % 2 == 0) number--;   // Make number odd
+            bool finded = false;  
+            bool findedFactor;
+            while(!finded){
+                findedFactor = true;
+                number+=2;
+                for(int i = 3;i <= Math.Sqrt(number); i += 2){    // ----TODO: Replace with our Sqrt function
+                    if(number % i == 0) {
+                        findedFactor = true;
+                        break;
+                    }
+                }
+                if(findedFactor){
+                        finded = true;
+                }
+            }
+            return number;    
+        }
     }
 }
