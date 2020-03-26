b10d617d53ba7ab46069b98966006bea2b7e1e68
RivolaCz <m.rivola@seznam.cz>
2020-03-26 21:48:29 +0100

    Updated prime number function and added test on prime numbers

--- src/MathLib/MathClass.cs
+++ src/MathLib/MathClass.cs
@@ -4,10 +4,11 @@ namespace MathLib
 {
     public static class MathClass
     {
-        //Zde doplňte vaše matematické funkce
-        //Return first prime number after given number.
+        // Zde doplňte vaše matematické funkce
+
+        // Return first prime number after given number.
         public static long FirstPrimeNumberAfterNumber(long number){
-            if(number < 1) number = 1;  //Negative numbers are not prime numbers
+            if (number < 2) return 2; // 2 is first prime number
             if(number % 2 == 0) number--;   // Make number odd
             bool finded = false;  
             bool findedFactor;
@@ -16,7 +17,7 @@ namespace MathLib
                 number+=2;
                 for(int i = 3;i <= Math.Sqrt(number); i += 2){    // ----TODO: Replace with our Sqrt function
                     if(number % i == 0) {
-                        findedFactor = true;
+                        findedFactor = false;
                         break;
                     }
                 }
--- src/MathLibraryTests/UnitTest1.cs
+++ src/MathLibraryTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLib;
 
 namespace MathLibraryTests
 {
@@ -9,6 +10,21 @@ namespace MathLibraryTests
         public void TestMethod1()
         {
             Assert.IsFalse(false);
+        }
+
+        [TestMethod]
+        public void PrimeNumberTest()
+        {
+            Assert.Equals(2, MathClass.FirstPrimeNumberAfterNumber(-99999));
+            Assert.Equals(2, MathClass.FirstPrimeNumberAfterNumber(0));
+            Assert.Equals(2, MathClass.FirstPrimeNumberAfterNumber(1));
+            Assert.Equals(3, MathClass.FirstPrimeNumberAfterNumber(2));
+            Assert.Equals(5, MathClass.FirstPrimeNumberAfterNumber(3));
+            Assert.Equals(5, MathClass.FirstPrimeNumberAfterNumber(4));
+            Assert.Equals(29, MathClass.FirstPrimeNumberAfterNumber(25));
+            Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(139));
+            Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(141));
+            Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(144));
         }
     }
 }
