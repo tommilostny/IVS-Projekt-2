733cd9f705f270d0c3ca53f39a179514e6e79fff
tommilostny <tommilostny@live.com>
2020-04-02 20:16:22 +0200

    added factorial unit test, factorial tests

--- src/MathLibraryTests/UnitTest1.cs
+++ src/MathLibraryTests/UnitTest1.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MathLib;
+using System;
 
 namespace MathLibraryTests
 {
     [TestClass]
-    public class UnitTest1
+    public class PrimeNumber_UnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.IsFalse(false);
-        }
-
         [TestMethod]
         public void PrimeNumberTest()
         {
@@ -25,6 +20,27 @@ namespace MathLibraryTests
             Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(139));
             Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(141));
             Assert.Equals(149, MathClass.FirstPrimeNumberAfterNumber(144));
+        }
+    }
+
+    [TestClass]
+    public class Factorial_UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FactorialNegatives_Test()
+        {
+            MathClass.Factorial(-1);
+        }
+
+        [TestMethod]
+        public void FactorialResults_Test()
+        {
+            long[] numbers = { 0, 1, 2, 3, 4, 5, 6, 11, 20 };
+            long[] results = { 1, 1, 2, 6, 24, 120, 720, 39916800, 2432902008176640000 };
+
+            for (int i = 0; i < numbers.Length; i++)
+                Assert.AreEqual(results[i], MathClass.Factorial(numbers[i]));
         }
     }
 }
