91c8bd82dbdd880846b3873f302808417f0d80b3
tommilostny <tommilostny@live.com>
2020-04-09 23:24:53 +0200

    profiling project initialization

---
+++ src/Profiling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathLib;
+
+namespace Profiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            List<double> x = new List<double>();
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                x.Add(Convert.ToDouble(line));
+            }
+
+            double s; //výsledná směrodatná odchylka
+            int N = x.Count; //počet načtených hodnot x
+        }
+    }
+}
